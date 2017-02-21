1251080897ce0bca8f896fc649d37ac3b2f783bf
Farhan Syed <fasyed2@gmail.com>
2017-02-20 20:57:50 -0600

    Create stream-multiple.sh

---
+++ stream-multiple.sh
@@ -0,0 +1,17 @@
+#sl logo fb and yt with tee muxer
+ffmpeg \
+-loop 1 -thread_queue_size 512 -i sl.png \
+-thread_queue_size 512 -f avfoundation -i ":0" \
+-flags +global_header \
+-r 30 -g 60 -keyint_min 60 \
+-c:v libx264 -pix_fmt yuv420p \
+-x264opts "keyint=60:min-keyint=60:no-scenecut" \
+-s 'hd720' -tune zerolatency \
+-c:a aac -b:a 128k  -ar 44100 \
+-f tee -map 0:v -map 1:a \
+"[f=flv]\
+rtmp://a.rtmp.youtube.com/live2/$KEY1|\
+[f=flv]\
+rtmp://rtmp-api.facebook.com:80/rtmp/$KEY2|\
+[f=flv]\
+rtmp://live-ord.twitch.tv/app/live_$KEY3"
